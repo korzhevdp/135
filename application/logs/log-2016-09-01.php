1f00baacfebb74af3991af9d04e6de6c925ce221
korzhevdp <korzhevdp@gmail.com>
2017-02-15 17:35:51 +0300

    v. 045 total commit

---
+++ application/logs/log-2016-09-01.php
@@ -0,0 +1,102 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2016-09-01 11:57:08 --> Config Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Hooks Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Utf8 Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> UTF-8 Support Disabled
+DEBUG - 2016-09-01 11:57:08 --> URI Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Router Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Output Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Security Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Input Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Global POST and COOKIE data sanitized
+DEBUG - 2016-09-01 11:57:08 --> Language Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Loader Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Database Driver Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Session Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Helper loaded: string_helper
+DEBUG - 2016-09-01 11:57:08 --> Session routines successfully run
+DEBUG - 2016-09-01 11:57:08 --> Controller Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Model Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Model Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Model Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Model Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> File loaded: application/views/iptemplate.php
+DEBUG - 2016-09-01 11:57:08 --> File loaded: application/views/restemplate.php
+DEBUG - 2016-09-01 11:57:08 --> File loaded: application/views/restemplate.php
+DEBUG - 2016-09-01 11:57:08 --> File loaded: application/views/restemplate.php
+DEBUG - 2016-09-01 11:57:08 --> File loaded: application/views/restemplate.php
+DEBUG - 2016-09-01 11:57:08 --> File loaded: application/views/reslisttemplate.php
+DEBUG - 2016-09-01 11:57:08 --> File loaded: application/views/menu/navigation.php
+DEBUG - 2016-09-01 11:57:08 --> File loaded: application/views/user_details.php
+DEBUG - 2016-09-01 11:57:08 --> File loaded: application/views/page_footer.php
+DEBUG - 2016-09-01 11:57:08 --> File loaded: application/views/page_container.php
+DEBUG - 2016-09-01 11:57:08 --> Final output sent to browser
+DEBUG - 2016-09-01 11:57:08 --> Total execution time: 0.0639
+DEBUG - 2016-09-01 11:57:08 --> Config Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Hooks Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Utf8 Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> UTF-8 Support Disabled
+DEBUG - 2016-09-01 11:57:08 --> URI Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Router Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Output Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Security Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Input Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Global POST and COOKIE data sanitized
+DEBUG - 2016-09-01 11:57:08 --> Language Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Loader Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Database Driver Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Session Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Helper loaded: string_helper
+DEBUG - 2016-09-01 11:57:08 --> Session routines successfully run
+DEBUG - 2016-09-01 11:57:08 --> Controller Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Model Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Model Class Initialized
+DEBUG - 2016-09-01 11:57:08 --> Final output sent to browser
+DEBUG - 2016-09-01 11:57:08 --> Total execution time: 0.0259
+DEBUG - 2016-09-01 11:57:17 --> Config Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Hooks Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Utf8 Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> UTF-8 Support Disabled
+DEBUG - 2016-09-01 11:57:17 --> URI Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Router Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Output Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Security Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Input Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Global POST and COOKIE data sanitized
+DEBUG - 2016-09-01 11:57:17 --> Language Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Loader Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Database Driver Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Session Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Helper loaded: string_helper
+DEBUG - 2016-09-01 11:57:17 --> Session routines successfully run
+DEBUG - 2016-09-01 11:57:17 --> Controller Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Model Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Model Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Model Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Model Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Database Driver Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Language file loaded: language/english/profiler_lang.php
+DEBUG - 2016-09-01 11:57:17 --> Helper loaded: text_helper
+DEBUG - 2016-09-01 11:57:17 --> Final output sent to browser
+DEBUG - 2016-09-01 11:57:17 --> Total execution time: 0.0393
+DEBUG - 2016-09-01 11:57:17 --> Config Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Hooks Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Utf8 Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> UTF-8 Support Disabled
+DEBUG - 2016-09-01 11:57:17 --> URI Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Router Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> No URI present. Default controller set.
+DEBUG - 2016-09-01 11:57:17 --> Output Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Security Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Input Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Global POST and COOKIE data sanitized
+DEBUG - 2016-09-01 11:57:17 --> Language Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Loader Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Database Driver Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Session Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Helper loaded: string_helper
+DEBUG - 2016-09-01 11:57:17 --> A session cookie was not found.
+DEBUG - 2016-09-01 11:57:17 --> Session routines successfully run
+DEBUG - 2016-09-01 11:57:17 --> Controller Class Initialized
+DEBUG - 2016-09-01 11:57:17 --> Helper loaded: url_helper
